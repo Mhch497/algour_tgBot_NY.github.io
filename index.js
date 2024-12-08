e41be3d345f6a83cf6186fa1f6546ab44446eb68
Mhch497 <mhch41997@gmail.com>
2024-12-08 21:41:48 +0500

    added py file

---
+++ index.js
@@ -0,0 +1,19 @@
+let tg = window.Telegram.WebApp;
+
+
+// tg.initData - получение данных от пользователя как строку 
+// tg.initDataUnsafe - получение данных от пользователя как объект
+// tg.isExpanded - проверяет открыто ли мини-приложение полностью по высоте (true/false)
+// tg.sendData(data) - отправка данных из приложения (в нашем случае из полей input)
+// tg.expand() - растянет окно во всю высоту
+// tg.close() - закрытие приложения
+
+// Вот примеры как получать данные пользователя с помощью объекта tg:
+
+// tg.initDataUnsafe.user.username - получение username
+
+// tg.initDataUnsafe.user.first_name - получение first_name юзера
+
+// Важно отметить, что такие параметры как initData и initDataUnsafe работают, 
+// только если их запустить из меню команд бота, а sendData - только если через keyboard button. 
+// Под это приходится адаптироваться.
